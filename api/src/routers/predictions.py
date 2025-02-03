466bcf5c4159435086b3bfab466d3dad5e4bdcdb
David Cadena <david.cadena@hotmail.com>
2025-02-03 16:31:28 -0500

    ✨ Feat: Resolve PR comments

--- api/src/routers/predictions.py
+++ api/src/routers/predictions.py
@@ -2,19 +2,19 @@ from fastapi import Depends, APIRouter, HTTPException
 
 from factories.use_cases import create_llm_prediction_use_case
 from core.src.use_cases import CreatePrediction
-from ..dtos import PredictionRequest
 
 predictions_router = APIRouter()
 OPENAI_ASSISTANT_NAME = "marketing"
 
 
-@predictions_router.post("/")
+@predictions_router.get("/home-team/{home_team_id}/away-team/{away_team_id}")
 async def create_prediction(
-    request: PredictionRequest,
+    home_team_id: str,
+    away_team_id: str,
     use_case: CreatePrediction = Depends(create_llm_prediction_use_case),
 ):
     try:
-        prediction = use_case.run(request.teams_data, OPENAI_ASSISTANT_NAME)
+        prediction = use_case(OPENAI_ASSISTANT_NAME, home_team_id, away_team_id)
         return prediction
 
     except Exception as e:
--- core/src/use_cases/create_llm_prediction.py
+++ core/src/use_cases/create_llm_prediction.py
@@ -5,7 +5,8 @@ class CreatePrediction:
     def __init__(self, ai_assistant_repository: AIAssistantRepository):
         self.ai_assistant_repository = ai_assistant_repository
 
-    def run(self, teams_data: str, assistant_name: str):
+
+    def __call__(self, assistant_name: str, home_team_id: str, away_team_id: str):
         try:
             assistant = self.ai_assistant_repository.get_assistant(assistant_name=assistant_name)
 
@@ -16,7 +17,7 @@ class CreatePrediction:
             self.ai_assistant_repository.add_message(
                 thread_id=thread_id.id, 
                 role="user", 
-                message=teams_data
+                message=f"Add"
             )
 
             run = self.ai_assistant_repository.create_and_poll(
@@ -27,7 +28,6 @@ class CreatePrediction:
                 raise Exception(f"The status is: {run.status}")
             
             messages = self.ai_assistant_repository.list_messages(thread_id=thread_id.id)
-            print(messages, thread_id)
 
             return messages
         except Exception as e:
